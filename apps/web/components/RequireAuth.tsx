54f6719006077dbd4e339d478055db9955c7880f
Shivam Rana <4826shivam@gmail.com>
2025-09-19 22:49:41 +0530

    auth: authentication, setUser at login page

---
+++ apps/web/components/RequireAuth.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useUserStore } from "@repo/store";
+import Loading from "./ui/Loading";
+import { useSession } from "../lib/auth-client";
+
+export function RequireAuth({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { data: session, isPending } = useSession();
+  const { setUser } = useUserStore();
+
+  useEffect(() => {
+    if (!isPending && !session) {
+      router.push("/login");
+      return;
+    }
+    if (session && session.user) {
+      setUser(session.user);
+    }
+  }, [session, isPending, setUser, router]);
+
+  if (isPending) {
+    return <Loading />;
+  }
+  return <>{children}</>;
+}
