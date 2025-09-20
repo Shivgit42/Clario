0e9a3f1fac9b173de03465090941efa842601446
Shivam Rana <4826shivam@gmail.com>
2025-09-20 18:09:49 +0530

    refactor: code layout

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import manifest from "./manifest.json";
 import path from "path";
 
