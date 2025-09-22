aa04cc460af65f25b4c769a35e365521fb127357
Shivam Rana <4826shivam@gmail.com>
2025-09-22 15:44:49 +0530

    feat: added more flexibility to accept youtube thumnails, google avatar and any image

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,9 +2,31 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      // YouTube thumbnails
+      { protocol: "https", hostname: "img.youtube.com", pathname: "/vi/**" },
+      { protocol: "https", hostname: "i.ytimg.com", pathname: "/vi/**" },
+
+      // Google user avatars
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh4.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh1.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh2.googleusercontent.com",
+        pathname: "/**",
       },
     ],
   },
