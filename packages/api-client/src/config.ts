7e90eb19564aaeefbe0c0f6b03147cf4512969bc
Shivam Rana <4826shivam@gmail.com>
2025-10-15 13:09:22 +0530

    fix: request going from clario.shivamte.me in local dev while landing to dashboard causing cors origin error, but now it is fixed

--- packages/api-client/src/config.ts
+++ packages/api-client/src/config.ts
@@ -1,1 +1,16 @@
-export const API_URL = "https://clario.shivamte.me/api";
+const isExtension =
+  typeof window !== "undefined" &&
+  typeof (window as any).chrome !== "undefined" &&
+  (window as any).chrome.runtime &&
+  (window as any).chrome.runtime.id;
+
+const isDevelopment =
+  !isExtension &&
+  typeof window !== "undefined" &&
+  window.location.hostname === "localhost";
+
+export const API_URL = isExtension
+  ? "https://clario.shivamte.me/api"
+  : isDevelopment
+    ? "http://localhost:3000/api"
+    : "https://clario.shivamte.me/api";
